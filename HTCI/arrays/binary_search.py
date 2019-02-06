657a62d3ca9b3fe1e4a15757f97ce6b1c45af7eb
ntkjer <ntkjer@gmail.com>
2019-02-05 19:05:56 -0800

    added iterative binary search solution

--- HTCI/arrays/binary_search.py
+++ HTCI/arrays/binary_search.py
@@ -1,6 +1,6 @@
 #a is sorted list
 def binary_search(a, key):
-  #TODO: Write - Your - Code
+  #Run in O(log n) but has memory complexity of O(log n)
   return binary_search_helper(a, key, 0, len(a)-1)
 
 
@@ -14,3 +14,15 @@ def binary_search_helper(a, key, low, high):
   else:
     return binary_search_helper(a, key, mid + 1, high)
  
+ def binary_search_iterative(a, key):
+  low = 0
+  high = len(a) - 1
+  while low < high:
+    mid = low + ((high - low) // 2)
+    if a[mid] == key: 
+      return mid
+    elif key < a[mid]:
+      high = mid - 1
+    else:
+      low = mid + 1
+  return -1
